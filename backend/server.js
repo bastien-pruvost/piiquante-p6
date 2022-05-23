16116d562734d8fb1ced580f2df5de05e2d0bfa6
Bastien Pruvost <bastien.mory@gmail.com>
2022-05-23 21:16:49 +0200

    Add express and dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-  res.end('Voila la reponse');
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3000;
+process.env.NODE_ENV = 'development';
+
+server.listen(port, () => {
+  console.log(`Environment : ${process.env.NODE_ENV}`);
+  console.log(`Listening on port ${port}`);
 });
-
-server.listen(process.env.PORT || 3000);
