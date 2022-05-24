670ce5f6a346951d0f7e38b4fb740297f7c8b9ce
Bastien Pruvost <bastien.mory@gmail.com>
2022-05-25 01:14:51 +0200

    Add token in login middleware

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const usersQueries = require('../queries/users.queries');
 const argon2 = require('../configs/argon2.config');
+const { createToken } = require('../configs/jwt.config');
 
 exports.signup = async (req, res, next) => {
   try {
@@ -12,8 +13,9 @@ exports.signup = async (req, res, next) => {
 
 exports.login = async (req, res, next) => {
   try {
-    const user = await usersQueries.findUser(req.body);
+    const user = await usersQueries.findUserByEmail(req.body.email);
     if (!user) {
+      // throw new Error('Utilisateur non trouvé')
       return res.status(401).json({ error: '401 : Utilisateur non trouvé' });
     }
     const passwordIsValid = await argon2.verifyPassword(req.body.password, user.password);
@@ -21,8 +23,8 @@ exports.login = async (req, res, next) => {
       return res.status(401).json({ error: '401 : Mot de passe incorrect' });
     }
     return res.status(200).json({
-      userId: user._id,
-      token: 'TOKENISOK',
+      userId: user._id.toString(),
+      token: createToken(user),
     });
   } catch (err) {
     return res.status(500).json({ error: `500 : ${err.message}` });
