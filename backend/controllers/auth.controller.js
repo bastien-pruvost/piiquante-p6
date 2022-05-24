bd04df5a65902cb5d1de60f75d95a84b358e2af8
Bastien Pruvost <bastien.mory@gmail.com>
2022-05-24 12:25:37 +0200

    Refactoring errors

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -6,7 +6,7 @@ exports.signup = async (req, res, next) => {
     await usersQueries.createUser(req.body);
     return res.status(201).json({ message: 'Utilisateur créé' });
   } catch (err) {
-    return res.status(500).json({ error: err });
+    return res.status(500).json({ error: `500 : ${err.message}` });
   }
 };
 
@@ -14,17 +14,17 @@ exports.login = async (req, res, next) => {
   try {
     const user = await usersQueries.findUser(req.body);
     if (!user) {
-      return res.status(401).json({ error: 'Utilisateur non trouvé' });
+      return res.status(401).json({ error: '401 : Utilisateur non trouvé' });
     }
     const passwordIsValid = await argon2.verifyPassword(req.body.password, user.password);
     if (!passwordIsValid) {
-      return res.status(401).json({ error: 'Mot de passe incorrect' });
+      return res.status(401).json({ error: '401 : Mot de passe incorrect' });
     }
     return res.status(200).json({
       userId: user._id,
       token: 'TOKENISOK',
     });
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
+  } catch (err) {
+    return res.status(500).json({ error: `500 : ${err.message}` });
   }
 };
