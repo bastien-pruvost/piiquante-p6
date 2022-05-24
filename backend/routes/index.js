5800c2759f5f003aa6e99a15ad111dbb13437aef
Bastien Pruvost <bastien.mory@gmail.com>
2022-05-25 01:12:32 +0200

    Refactoring routes index

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const authRoutes = require('./auth.routes');
 const saucesRoutes = require('./sauces.routes');
+const { ensureAuthenticated } = require('../middlewares/security.middleware');
 
-// --- ROUTERS ---
+// --- ROUTERS /API ---
 router.use('/auth', authRoutes);
 router.use('/sauces', saucesRoutes);
 
