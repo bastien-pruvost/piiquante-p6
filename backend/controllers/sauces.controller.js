fb43459f0e629c24915b882345960ae76ac17826
Bastien Pruvost <bastien.mory@gmail.com>
2022-05-25 01:15:31 +0200

    Prepare sauces routes and controllers

--- backend/controllers/sauces.controller.js
+++ backend/controllers/sauces.controller.js
@@ -1,1 +1,13 @@
+const saucesQueries = require('../queries/users.queries');
 
+exports.getAllSauces = async (req, res, next) => '';
+
+exports.getOneSauce = async (req, res, next) => '';
+
+exports.sauceCreate = async (req, res, next) => '';
+
+exports.sauceModify = async (req, res, next) => '';
+
+exports.sauceDelete = async (req, res, next) => '';
+
+exports.sauceLike = async (req, res, next) => '';
--- backend/routes/sauces.routes.js
+++ backend/routes/sauces.routes.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const saucesCtrl = require('../controllers/sauces.controller');
+const { ensureAuthenticated } = require('../middlewares/security.middleware');
 
-router.use('/', (req, res, next) => {
-  res.status(200).json({ message: 'ok' });
-});
-router.use('/:id', (req, res, next) => {
-  res.status(200).json({ message: 'ok' });
-});
+// --- ROUTERS /API/SAUCES ---
+router.get('/', saucesCtrl.getAllSauces);
+router.get('/:id', saucesCtrl.getOneSauce);
+router.post('/', saucesCtrl.sauceCreate);
+router.put('/:id', saucesCtrl.sauceModify);
+router.delete('/:id', saucesCtrl.sauceDelete);
+router.post('/:id/like', saucesCtrl.sauceLike);
 
 module.exports = router;
