d2bce95f2c558cf2a8f3c8af4f5d95680311a325
Bastien Pruvost <bastien.mory@gmail.com>
2022-05-26 00:02:25 +0200

    Add sauce model

--- backend/models/sauce.model.js
+++ backend/models/sauce.model.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
-const sauceSchema = mongoose.Schema({});
+const sauceSchema = mongoose.Schema({
+  userId: { type: String, required: true },
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, min: 1, max: 10, required: true },
+  likes: { type: Number, default: 0 },
+  dislikes: { type: Number, default: 0 },
+  usersLiked: { type: [String] },
+  usersDisliked: { type: [String] },
+});
 
 sauceSchema.plugin(uniqueValidator);
 
