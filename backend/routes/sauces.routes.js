32003165537062bc1b556bee1863bdf110babf19
Bastien Pruvost <bastien.mory@gmail.com>
2022-05-26 02:58:14 +0200

    Change name of routes

--- backend/routes/sauces.routes.js
+++ backend/routes/sauces.routes.js
@@ -3,11 +3,11 @@ const saucesCtrl = require('../controllers/sauces.controller');
 const { ensureAuthenticated } = require('../middlewares/security.middleware');
 
 // --- ROUTERS /API/SAUCES ---
-router.get('/', saucesCtrl.getAllSauces);
-router.get('/:id', saucesCtrl.getOneSauce);
-router.post('/', saucesCtrl.sauceCreate);
-router.put('/:id', saucesCtrl.sauceModify);
-router.delete('/:id', saucesCtrl.sauceDelete);
-router.post('/:id/like', saucesCtrl.sauceLike);
+router.get('/', ensureAuthenticated, saucesCtrl.getAllSauces);
+router.get('/:id', ensureAuthenticated, saucesCtrl.getOneSauce);
+router.post('/', ensureAuthenticated, saucesCtrl.createSauce);
+router.put('/:id', ensureAuthenticated, saucesCtrl.modifySauce);
+router.delete('/:id', ensureAuthenticated, saucesCtrl.deleteSauce);
+router.post('/:id/like', ensureAuthenticated, saucesCtrl.likeSauce);
 
 module.exports = router;
