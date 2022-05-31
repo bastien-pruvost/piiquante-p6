84aeb0a906c8b2f2861c8255042da011b6ed73f8
Bastien Pruvost <bastien.mory@gmail.com>
2022-05-31 14:05:33 +0200

    Remove rateLimiter of signup request

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const authCtrl = require('../controllers/auth.controller');
 const { signupValidator } = require('../middlewares/validator.middleware');
-const { rateLimiter } = require('../middlewares/limiter.middleware');
+const { loginLimiter } = require('../middlewares/limiter.middleware');
 
 // --- ROUTERS /API/AUTH ---
-router.post('/signup', rateLimiter, signupValidator, authCtrl.signup);
-router.post('/login', rateLimiter, authCtrl.login);
+router.post('/signup', signupValidator, authCtrl.signup);
+router.post('/login', loginLimiter, authCtrl.login);
 
 module.exports = router;
