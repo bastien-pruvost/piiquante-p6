634900d6711d98564c0ab126ad5e2f93cd3b2dc4
Bastien Pruvost <bastien.mory@gmail.com>
2022-06-01 18:18:12 +0200

    Change jwt secret key env variable

--- backend/configs/jwt.config.js
+++ backend/configs/jwt.config.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
 
 // Function to create a token that contains id of the user passed as argument
-exports.createToken = (user) => jwt.sign({ userId: user._id.toString() }, process.env.JWT_KEY, { expiresIn: '6h' });
+exports.createToken = (user) =>
+  jwt.sign({ userId: user._id.toString() }, process.env.JWT_SECRET_KEY, { expiresIn: '6h' });
 
 // Function to decode a token and retrieve the userId it contains
-exports.verifyToken = (token) => jwt.verify(token, process.env.JWT_KEY);
+exports.verifyToken = (token) => jwt.verify(token, process.env.JWT_SECRET_KEY);
