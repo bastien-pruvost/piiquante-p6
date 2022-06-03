9ea900a62049c156a3159d00b6948ba9c8c2eaa6
Bastien Pruvost <bastien.mory@gmail.com>
2022-06-03 15:59:10 +0200

    Remove try catch from users queries

--- backend/queries/users.queries.js
+++ backend/queries/users.queries.js
@@ -1,34 +1,13 @@
 const User = require('../models/user.model');
-const argon = require('../configs/argon2.config');
 
 // Database query to save a new user
-exports.createUser = async (user) => {
-  try {
-    const hashedPassword = await argon.hashPassword(user.password);
-    const newUser = new User({
-      email: user.email,
-      password: hashedPassword,
-    });
-    return newUser.save();
-  } catch (err) {
-    throw new Error(`Error when create user in DB : ${err}`);
-  }
+exports.createUser = (email, hashedPassword) => {
+  const newUser = new User({ email, password: hashedPassword });
+  return newUser.save();
 };
 
 // Database query to find a user with his email
-exports.findUserByEmail = async (email) => {
-  try {
-    return User.findOne({ email }).exec();
-  } catch (err) {
-    throw new Error(`Error when searching user by email in DB : ${err}`);
-  }
-};
+exports.findUserByEmail = (email) => User.findOne({ email }).exec();
 
 // Database query to find a user with his id
-exports.findUserById = async (id) => {
-  try {
-    return User.findOne({ _id: id }).exec();
-  } catch (err) {
-    throw new Error(`Error when searching user by id in DB : ${err}`);
-  }
-};
+exports.findUserById = (id) => User.findOne({ _id: id }).exec();
